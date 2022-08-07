6e8b2c5c4555f41113e0c523c6ff105fa1299134
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-08-07 17:28:53 +0300

    3ve4unKuvvetleri

--- src/CiftSayi.java
+++ src/CiftSayi.java
@@ -1,2 +1,21 @@
-public class CiftSayi {
+import java.util.Scanner;
+public class CiftSayi{
+    public static void main(String[] args) {
+        /*
+        Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift ve 4'ün
+        katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+
+         */
+        int number,total=0;
+        Scanner input=new Scanner(System.in);
+        do{
+            System.out.println("Bir Sayi Giriniz: ");
+            number = input.nextInt();
+            if ((number%4==0)&& (number%2==0)) {
+                total += number;
+            }
+        }
+        while(number%2==0);
+        System.out.println("Cift ve 4'un kati sayilarin toplami: "+ total);
+    }
 }
--- src/KuvvetAlma.java
+++ src/KuvvetAlma.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class KuvvetAlma {
+import java.util.Scanner;
+public class KuvvetAlma {
+    public static void main(String[] args) {
+        //Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.println("bir sayi giriniz");
+        number = input.nextInt();
+
+        System.out.println("Dordun Kuvvetleri; ");
+        for (int i=1; i<=number; i*=4){
+            System.out.println(i );
+        }
+
+        System.out.println("Besin Kuvvetleri; ");
+        for (int i=1; i<=number; i*=5){
+            System.out.println(i);
+        }
+
+    }
 }
