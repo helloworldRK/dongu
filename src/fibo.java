bddd5952ced50fe1685e94d2bc833e5bab33364d
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-08-11 16:26:45 +0300

    Update fibo.java

--- src/fibo.java
+++ src/fibo.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class fibo {
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kacinci elemana kadar yapilsin: ");
+        int n = input.nextInt();
+        int b=0, c=1 ,d;
+        for (int i=1;i<=n;i++){
+            d=b+c;
+            System.out.println(b+" + " +c+ " = "+d);
+            b=c;
+            c=d;
+            System.out.println(" ");
+        }
+
+
+
+
+
+
+    }
