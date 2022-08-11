200d2d94ded64ad6ec2586bdd2c0d183bc62af56
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-08-11 16:27:37 +0300

    Update harmonik.java

--- src/harmonik.java
+++ src/harmonik.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class harmonik {
+import java.util.Scanner;
+public class harmonik {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.println("N sayiniz : ");
+        int n= inp.nextInt();
+        double total=0;
+
+        for (int i=1;i<=n;i++){
+            total+=1.0/i;
+        }
+        System.out.println(total);
+    }
 }
